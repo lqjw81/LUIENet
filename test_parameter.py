e51827c21e2c90915c34285439585a94ac955f72
lqjw81 <154230181+lqjw81@users.noreply.github.com>
2024-11-03 21:39:33 +0800

    Delete test_parameter.py

--- test_parameter.py
+++
@@ -1,7 +0,0 @@
-# 创建一个简单CNN模型实例
-from archs.FasterUIE import FIVE_APLUSNet
-model = FIVE_APLUSNet()
-
-# 计算模型的参数数量
-total_params = sum(p.numel() for p in model.parameters())
-print("总参数数量：", total_params)
