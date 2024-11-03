68ffe20e4d1ec03a3269dcaa2dd6d1adfe37b62b
lqjw81 <154230181+lqjw81@users.noreply.github.com>
2024-11-03 21:50:34 +0800

    Update train.py

--- train.py
+++ train.py
@@ -21,7 +21,6 @@ from config.config import FLAGES
 
 torch.backends.cudnn.enabled = True
 torch.backends.cudnn.benchmark = True
-## 超参数设置
 satellite = 'Five'
 start_epochs = 0
 total_epochs = 500
@@ -43,7 +42,6 @@ ssim_loss = SSIMLoss()
 loss_per = PerceptualLoss()
 
 class DataPrefetcher():
-# 这个类使得在模型训练时可以异步加载下一个数据批次，以减少训练过程中的等待时间。
     def __init__(self, loader):
         self.loader = iter(loader)
         self.preload()
@@ -60,60 +58,46 @@ class DataPrefetcher():
         self.preload()
         return batch
 
-# 实例化模型
 model = FIVE_APLUSNet()
-# 将模型加载到设备上，用的cpu就放到cpu，gpu就放到gpu
+
 model.to(device)
-# 定义一个优化器，传入要更新的模型参数、超参数以及学习率等。
+
 optimizer = torch.optim.Adam(model.parameters(), lr=FLAGES.lr)
 scheduler = torch.optim.lr_scheduler.CyclicLR(optimizer, base_lr=0.0004, max_lr=1.2 * 0.0004,
                                               cycle_momentum=False)
-# optimizer = torch.optim.Adam(model.parameters(), lr=FLAGES.lr, betas=[0.9, 0.999])  # ,weight_decay=self.weight_decay)
-# optimizer = optim.Adam(model.parameters(), lr=FLAGES.lr, betas=(0.9, 0.999), eps=1e-8)
 
 warmup_epochs = 3
 scheduler_cosine = optim.lr_scheduler.CosineAnnealingLR(optimizer, total_epochs, eta_min=1e-6)
-# scheduler = GradualWarmupScheduler(optimizer, multiplier=1, total_epoch=warmup_epochs,
-#                                    after_scheduler=scheduler_cosine)
-
-# scheduler = torch.optim.lr_scheduler.CyclicLR(optimizer, base_lr=0.0004, max_lr=1.2 * FLAGES.lr, cycle_momentum=False)
-# @torchsnooper.snoop()
-# 模型训练
 def train(model, train_datasetloader, start_epoch):
     log = open("./logs.txt", mode="a", encoding="utf-8")
     best_psnr, best_ssim, best_epoch_ssim, best_epoch_psnr = 0., 0., 0., 0.
     print('===>Begin Training!')
-    # 如果模型中用到了BN和Dropout，则需要使用model.train()启用，
-    # 而在测试时怎用model.eval()关闭启用，以此保证BN层的均值和方差不变且不丢弃神经元。
     model.train()
-    steps_per_epoch = len(train_datasetloader) #len(datasetloader)就是我以batchsize大小要多少次才能遍历完数据集。
-    total_iterations = total_epochs * steps_per_epoch #遍历的总次数，total_epoch是要对数据集读取多少遍*没遍要以batchsize大小读多少次。
+    steps_per_epoch = len(train_datasetloader)。
+    total_iterations = total_epochs * steps_per_epoch 
     print('total_iterations:{}'.format(total_iterations))
     # train_loss_record = open('%s/train_loss_record.txt' % FLAGES.record_dir, "w")
     for epoch in range(start_epoch + 1, total_epochs + 1):
-        start = time.time()  # 记录每轮训练的开始时刻
-        prefetcher_train = DataPrefetcher(train_datasetloader)  # 预加载数据
+        start = time.time() 
+        prefetcher_train = DataPrefetcher(train_datasetloader)
         data = prefetcher_train.next()
         print('Fetching training UIEB spends {} seconds'.format(time.time()-start))
         while data is not None:
-            # 将数据放到设备上
             raw, label = data[0].to(device), data[1].to(device)
 
-            optimizer.zero_grad()  #清空梯度
+            optimizer.zero_grad() 
 
-            pred, predforvgg = model(raw)  #模型训练，获取结果
-
-            #计算损失
+            pred, predforvgg = model(raw) 
             loss_d = loss_f(pred, label)
             # loss_d = loss_f(pred, label)
             train_loss = loss_d+0.2*loss_per(pred, label)+0.5*ssim_loss(pred, label)+0.2*loss_per(predforvgg, label)
             # train_loss = ssim_loss(pred, label)+loss_d
 
-            train_loss.backward()  #反向传播
-            optimizer.step()  #更新梯度
+            train_loss.backward() 
+            optimizer.step() 
             scheduler.step()
 
-            data = prefetcher_train.next() #预加载数据
+            data = prefetcher_train.next() 
             print('=> {}-Epoch[{}/{}]: train_loss: {:.4f}'.format(satellite, epoch, total_epochs, train_loss.item(),))
 
         ## Evaluation
@@ -160,49 +144,6 @@ def train(model, train_datasetloader, start_epoch):
             torch.save(state, join(FLAGES.backup_model_dir, '{}-model-epochs{}.pth'.format(satellite, epoch)))
 
 
-        # backup a model every epoch
-        # if epoch % model_backup_freq == 0:  # 每间隔model_backup_freq轮保存一次权重。
-        #     model.eval()
-        #     PSNRs=[]
-        #     SSIMs=[]
-        #     prefetcher_val = DataPrefetcher(val_datasetloader)  # 预加载数据
-        #     data_val = prefetcher_val.next()
-        #     while data_val is not None:
-        #         raw, label = data_val[0].to(device), data_val[1].to(device)
-        #         pred, predforvgg = model(raw)
-        #         data_val = prefetcher_train.next()  # 预加载数据
-        #         with torch.no_grad():
-        #             for res, tar in zip(pred, label):
-        #                 temp_psnr = torchPSNR(res, tar)
-        #                 temp_ssim = torchSSIM(pred, label)
-        #                 PSNRs.append(temp_psnr)
-        #                 SSIMs.append(temp_ssim)
-        #
-        #     PSNRs = torch.stack(PSNRs).mean().item()
-        #     SSIMs = torch.stack(SSIMs).mean().item()
-        #
-        #     # Save the best PSNR model of validation
-        #     if PSNRs > best_psnr:
-        #         best_psnr = PSNRs
-        #         best_epoch_psnr = epoch
-        #         state = {'model': model.state_dict(), 'optimizer': optimizer.state_dict(), 'epoch': epoch}
-        #         torch.save(state, join(FLAGES.backup_model_dir, '{}-model-epochs{}-bestPSNR{}.pth'.format(satellite, epoch, best_epoch_psnr)))
-        #
-        #     print("[PSNR] {:.4f} [Best_PSNR] {:.4f} (epoch {})".format(PSNRs, best_psnr, best_epoch_psnr))
-        #
-        #     # Save the best SSIM model of validation
-        #     if SSIMs > best_ssim:
-        #         best_ssim = SSIMs
-        #         best_epoch_ssim = epoch
-        #         state = {'model': model.state_dict(), 'optimizer': optimizer.state_dict(), 'epoch': epoch}
-        #         torch.save(state, join(FLAGES.backup_model_dir, '{}-model-epochs{}-bestSSIM{}.pth'.format(satellite, epoch, best_ssim)))
-        #     print("[SSIM] {:.4f}  [Best_SSIM] {:.4f} (epoch {})".format(SSIMs, best_ssim, best_epoch_ssim))
-        #
-        #     # Save each epoches of model
-        #     state = {'model': model.state_dict(), 'optimizer': optimizer.state_dict(), 'epoch': epoch}
-        #     torch.save(state, join(FLAGES.backup_model_dir, '{}-model-epochs{}.pth'.format(satellite, epoch)))
-
-        # 输出每轮训练花费时间
         time_epoch = (time.time() - start)
         print('==>No:epoch {} training costs {:.4f}min'.format(epoch, time_epoch / 60))
 
